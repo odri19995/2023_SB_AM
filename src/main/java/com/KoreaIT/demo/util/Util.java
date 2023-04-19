5e55b8f808b43427de00e66bc7b547d5fa58eb66
odri19995 <odri19995@gmail.com>
2023-04-19 14:08:31 +0900

    result Data

--- src/main/java/com/KoreaIT/demo/util/Util.java
+++ src/main/java/com/KoreaIT/demo/util/Util.java
@@ -3,16 +3,20 @@ package com.KoreaIT.demo.util;
 public class Util {
 	public static boolean empty(Object obj) {
 		
-		if(obj == null) {
+		if (obj == null) {
 			return true;
 		}
 		
+//		if (obj instanceof String == false) {
+//			return true;
+//		}
+		
 		String str = (String) obj;
+		
 		return str.trim().length() == 0;
 	}
-	
-	public static Object f(String format, Object... args) {
+
+	public static String f(String format, Object... args) {
 		return String.format(format, args);
 	}
-
 }
